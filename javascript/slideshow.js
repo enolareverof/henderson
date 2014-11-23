4444c1be2c9bf6358f2d9a1348ec432e92321cf0
enolareverof <iamenolareverof@gmail.com>
2014-11-24 03:33:54 +0600

    re-design

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
 		run();
-
+		setInterval(function(){
+			run();
+		},6500);
 
 		//this is a good way
 		/*$(window).resize(function() {
@@ -30,150 +32,67 @@ $(document).ready(function(){
 		});
 });
 
-var mCondition = true;
-var firstTime = true;
+
+var slide1 = $('#slide1');
+var slide2 = $('#slide2');
+var slide3 = $('#slide3');
+var slide4 = $('#slide4');
+
+var slide = 1;
 
 // a function which changes slide
 function run(){
-
-	var amountOfSlide=$('.slide').length;
-	var slideWidth = $('.slide').width();
-
-	console.log(slideWidth);
-
-	var position=0;
-	var forward = true;
-	var slideNo = 1;
-
-
-	changeSlide();
-
-	setInterval(changeSlide,5000);
-
-	// a function that changes slide
-	function changeSlide(){
-		
-			move(slideNo);
-			if(forward==true){
-				position=position+slideWidth;
-				slideNo++;
-			}
-			if(forward==false){
-				position=position-slideWidth;
-				slideNo--;
-			}
-			if(position==slideWidth*(amountOfSlide-1) || position==0){
-				forward=!forward;
-			}
-	} // end of function change slide
-
-
-	// a function that moves slide
-	function move(slideNo){
-
-
-			$('.slide-holder').animate({'margin-left':-position},
-
-				function(){
-
-
-					if ( $(window).width() <= 1000 && firstTime==false) {
-						//$('.window .mission').animate({'top':'-55%'});
-						//console.log("hi")
-					}
-
-
-					if(firstTime==false){
-						if(mCondition==true){
-							if ( $(window).width() > 1000) {
-								$('.window .slide-holder .slide .slidetext').animate({'padding-left': '7%'});
-								$('.window .slide-holder .slide .slidetext').animate({'padding-right':'33%'});
-								// $('.window .mission').animate({'margin-left':'785'});
-								$('.window .mission').css({
-									'transform':'translate3d(850px,0px,0px)'
-								});
-								mCondition=!mCondition;
-							}
-						}
-						else{
-							if ( $(window).width() > 1000) {
-								$('.window .slide-holder .slide .slidetext').animate({'padding-left':'33%'});
-								$('.window .slide-holder .slide .slidetext').animate({'padding-right':'7%'});
-								// $('.window .mission').animate({'margin-left':'0'});
-								$('.window .mission').css({
-									'transform':'translate3d(0px,0px,0px)'
-								});
-								mCondition=!mCondition;
-							}
-						}
-					}
-					firstTime=false;
-
-
-
-					var beforeSlide=slideNo-1;
-					var afterSlide=slideNo+1;
-					if(beforeSlide<1){
-						beforeSlide=amountOfSlide;
-					}
-					if(afterSlide>3){
-						afterSlide=1;
-					}
-					
-
-					$('#slide'+slideNo+' .slidetext').animate(
-						{'top':'55%'},function(){
-							fade_in(
-								$(this).find('h3'),
-								$(this).find('p'),
-								$(this).find('a')
-							)
-						}
-					);
-					
-					$('#slide'+beforeSlide+' .slidetext').animate(
-						{'top':'100%'},function(){
-							fade_out(
-								$(this).find('h3'),
-								$(this).find('p'),
-								$(this).find('a')
-							)
-						}
-					);
-
-
-					$('#slide'+afterSlide+' .slidetext').animate(
-						{'top':'100%'},function(){
-							fade_out(
-								$(this).find('h3'),
-								$(this).find('p'),
-								$(this).find('a')
-							)
-						}
-					);
-
-
-				}
-			);
-	} // end of function move slide
-
-
-	// function to fadeIn
-	function fade_in(one,two,three){
-		one.fadeIn("fast",function(){
-			two.fadeIn("fast",function(){
-				three.fadeIn("fast");
-			});
-		});
+	if(slide===1){
+		Slide1();
 	}
-
-	// function to fadeOut
-	function fade_out(one,two,three){
-		one.fadeOut(1,function(){
-			two.fadeOut(1,function(){
-				three.fadeOut(1);
-			});
-		});
+	else if(slide===2){
+		Slide2();
 	}
+	else if(slide===3){
+		Slide3();
+	}
+	else{
+		Slide4();
+	}
+	slide++;
+	if(slide===5){
+		slide=1;
+	}
+}
+
+
+function Slide1(){
+	slide1.fadeIn(1500);
+	
+	slide2.fadeOut(1500);
+	slide3.fadeOut(1500);
+	slide4.fadeOut(1500);
+}
+
+
+function Slide2(){
+	slide2.fadeIn(1500);
+
+	slide1.fadeOut(1500);
+	slide3.fadeOut(1500);
+	slide4.fadeOut(1500);
+}
+
+
+function Slide3(){
+	slide3.fadeIn(1500);
+
+	slide1.fadeOut(1500);
+	slide2.fadeOut(1500);
+	slide4.fadeOut(1500);
+}
+
+
+function Slide4(){
+	slide4.fadeIn(1500);
+	
+	slide1.fadeOut(1500);
+	slide2.fadeOut(1500);
+	slide3.fadeOut(1500);
 }
 // end of function run
